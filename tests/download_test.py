ec8421e26ea49e6d79da2b094cc1affff972a354
SergiSolis <65497439+SergiSolis@users.noreply.github.com>
2022-04-26 19:02:16 +0200

    download_test.py  Corrected
    estaría bien que todo el contenido que descargamos vaya a la carpeta data

--- tests/download_test.py
+++ tests/download_test.py
@@ -1,3 +1,5 @@
+import os
+
 import freesound
 
 client = freesound.FreesoundClient()
@@ -6,5 +8,6 @@ client.set_token("xbdYSYi9lDMRwSekK8ThcIAe7gserici1pz0VoPe","token")
 results = client.text_search(query="trumpet",fields="id,name,previews")
 
 for sound in results:
-    sound.retrieve_preview("/tests/",sound.name+".mp3")
+    path_save = os.path.normpath(os.getcwd() + os.sep + os.pardir + "/data/") #to download all the content in the data folder
+    sound.retrieve_preview(path_save,sound.name+".mp3")
     print(sound.name)
