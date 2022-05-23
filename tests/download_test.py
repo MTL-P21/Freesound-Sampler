86c32978d9788e9b722b29bcb497036f2d46cfcf
SergiSolis <65497439+SergiSolis@users.noreply.github.com>
2022-05-23 20:36:34 +0200

    search menu + sampler fusion

--- tests/download_test.py
+++ tests/download_test.py
@@ -5,7 +5,7 @@ import freesound
 client = freesound.FreesoundClient()
 client.set_token("xbdYSYi9lDMRwSekK8ThcIAe7gserici1pz0VoPe","token")
 
-results = client.text_search(query="trumpet",fields="id,name,previews")
+results = client.text_search(query="trumpet",filter="ac_single_event:true", fields="id,name,previews")
 
 for sound in results:
     path_save = os.path.normpath(os.getcwd() + os.sep + os.pardir + "/data/") #to download all the content in the data folder
