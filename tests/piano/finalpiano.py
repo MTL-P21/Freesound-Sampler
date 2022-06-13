3c96f9d2407a9cce53325efd1272327821f9031a
bsanchezzz <blastemperance@BS.local>
2022-06-13 11:20:06 +0200

    Al revés

--- tests/piano/finalpiano.py
+++ tests/piano/finalpiano.py
@@ -25,32 +25,32 @@ key_to_note = {
         # on: a boolean to keep track of if this note is on
 
         #1st octave
-        pygame.K_q: (12, False, False),  # C0
-        pygame.K_2: (13, True, False),  # C#/Db0
-        pygame.K_w: (14, False, False),  # D0
-        pygame.K_3: (15, True, False),  # D#/Eb0
-        pygame.K_e: (16, False, False),  # E0
-        pygame.K_r: (17, False, False),  # F0
-        pygame.K_5: (18, True, False),  # F#/Gb0
-        pygame.K_t: (19, False, False),  # G0
-        pygame.K_6: (20, True, False),  # G#/Ab0
-        pygame.K_y: (21, False, False),  # A0
-        pygame.K_7: (22, True, False),  # A#/Bb0
-        pygame.K_u: (23, False, False),  # B0
+        pygame.K_z: (12, False, False),  # C0
+        pygame.K_s: (13, True, False),  # C#/Db0
+        pygame.K_x: (14, False, False),  # D0
+        pygame.K_d: (15, True, False),  # D#/Eb0
+        pygame.K_c: (16, False, False),  # E0
+        pygame.K_v: (17, False, False),  # F0
+        pygame.K_g: (18, True, False),  # F#/Gb0
+        pygame.K_b: (19, False, False),  # G0
+        pygame.K_h: (20, True, False),  # G#/Ab0
+        pygame.K_n: (21, False, False),  # A0
+        pygame.K_j: (22, True, False),  # A#/Bb0
+        pygame.K_m: (23, False, False),  # B0
 
         #2nd octave
-        pygame.K_z: (24, False, False),  # C1
-        pygame.K_s: (25, True, False),  # C#/Db1
-        pygame.K_x: (26, False, False),  # D1
-        pygame.K_d: (27, True, False),  # D#/Eb1
-        pygame.K_c: (28, False, False),  # E1
-        pygame.K_v: (29, False, False),  # F1
-        pygame.K_g: (30, True, False),  # F#/Gb1
-        pygame.K_b: (31, False, False),  # G1
-        pygame.K_h: (32, True, False),  # G#/Ab1
-        pygame.K_n: (33, False, False),  # A1
-        pygame.K_j: (34, True, False),  # A#/Bb1
-        pygame.K_m: (35, False, False),  # B1
+        pygame.K_q: (24, False, False),  # C1
+        pygame.K_2: (25, True, False),  # C#/Db1
+        pygame.K_w: (26, False, False),  # D1
+        pygame.K_3: (27, True, False),  # D#/Eb1
+        pygame.K_e: (28, False, False),  # E1
+        pygame.K_r: (29, False, False),  # F1
+        pygame.K_5: (30, True, False),  # F#/Gb1
+        pygame.K_t: (31, False, False),  # G1
+        pygame.K_6: (32, True, False),  # G#/Ab1
+        pygame.K_y: (33, False, False),  # A1
+        pygame.K_7: (34, True, False),  # A#/Bb1
+        pygame.K_u: (35, False, False),  # B1
 }
 
 def draw_keyboard(window) -> None:
